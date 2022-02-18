a258b98e16c6137df62c8883a06003d7815d4d82
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-18 11:02:55 -0700

    first commit

--- MethodOverloadingPractice/Program.cs
+++ MethodOverloadingPractice/Program.cs
@@ -6,7 +6,32 @@ namespace MethodOverloadingPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Add(2, 2);
+            Add(2.0m, 2.0m);
+            Add(3, 6, true);
+            Console.WriteLine(Add (3, 6, true));
         }
+
+        public static int Add(int x, int y)
+            {
+            return x + y;
+            }
+        public static decimal Add(decimal x, decimal y)
+            {
+                return x + y;
+        }
+        public static string Add(int x, int y, bool isPlural)
+        {
+            var sum = x + y;
+            if(sum == 1)
+            {
+                isPlural = true;
+                return ($"{sum} dollar");
+            }
+            else
+            {
+                return ($"{sum} dollars");
+            }
+         }
     }
 }
